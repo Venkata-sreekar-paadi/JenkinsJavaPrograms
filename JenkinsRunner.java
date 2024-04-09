d5186e6e54c117c64d15ef47a26d7f24a67664bf
sreekar <venkatasreekar.paadi@gmail.com>
2024-04-09 15:32:13 +0530

    jenkins java project commit

---
+++ JenkinsRunner.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class JenkinsRunner {
+
+	public static void main(String[] args) {
+		System.out.println("Hello this is our first Jav Program running through Jenkins");
+		System.out.println(new Date());
+	}
+
+}
